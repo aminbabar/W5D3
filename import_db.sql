b6fe681a4118a28d57207d4dd085fccb7b3ac7d0
aminbabar <amin.babar1@gmail.com>
2021-07-14 15:02:42 -0400

    easy questions complete

--- import_db.sql
+++ import_db.sql
@@ -1,21 +1,13 @@
-PRAGMA foreign_keys = ON;
-
-
-
--- DROP TABLE IF EXISTS plays;
-
--- CREATE TABLE plays (
---   id INTEGER PRIMARY KEY,
---   title TEXT NOT NULL,
---   year INTEGER NOT NULL,
---   playwright_id INTEGER NOT NULL,
-
---   FOREIGN KEY (playwright_id) REFERENCES playwrights(id)
--- );
-
 
 DROP TABLE if exists questions;
 DROP TABLE if exists users;
+DROP TABLE if exists question_follows;
+DROP TABLE if exists question_likes;
+DROP TABLE if exists replies;
+
+
+
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
@@ -37,7 +29,6 @@ CREATE TABLE questions (
 
 
 
-DROP TABLE if exists question_follows;
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
@@ -48,7 +39,6 @@ CREATE TABLE question_follows (
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
-DROP TABLE if exists replies;
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
@@ -64,7 +54,6 @@ CREATE TABLE replies (
 );
 
 
-DROP TABLE if exists question_likes;
 
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
@@ -79,14 +68,46 @@ CREATE TABLE question_likes (
 
 INSERT INTO users(fname, lname)
 VALUES
- ("Eddy", "Marshall"),
- ("Amin", "Babar");
+ ("Eddy", "Marshall"),  
+ ("Amin", "Babar"),
+ ("John", "Mendez"),
+ ("Casey", "Afleck"),
+ ("Jessica", "Chang");
 
 
- INSERT INTO questions(title, body, user_id)
- VALUES
-  ("NOTHING WORKS", "PRY: MY COMPUTER FAILS", (SELECT id FROM users WHERE fname = 'Eddy') ),
-  ("WEIRD ERROR", "WEIRD TERMINAL ERROR!!!! WHYYY", (SELECT id FROM users WHERE fname = 'Amin') );
+INSERT INTO questions(title, body, user_id)
+VALUES
+    ("NOTHING WORKS", "PRY: MY COMPUTER FAILS", (SELECT id FROM users WHERE fname = 'Eddy') ),
+    ("WEIRD ERROR", "WEIRD TERMINAL ERROR!!!! WHYYY", (SELECT id FROM users WHERE fname = 'Amin') );
+
+INSERT INTO replies(question_id, reply_id, user_id, body)
+VALUES
+    (1, null, 2, "Have you tried uninstalling?"),
+    (1, 1, 5, "How about napping!"),
+    (2, null, 4, "Show your code!!!!"),
+    (1, 2, 4, "SCREW YOU!");
+
+INSERT INTO question_follows(question_id, user_id)
+VALUES
+    (1, 1),
+    (1, 5),
+    (2, 2),
+    (1, 4),
+    (2, 3);
 
 
- 
+INSERT INTO question_likes(question_id, user_id)   --WE FLIPPED THE ARGUMENTS!!!
+VALUES
+    (2,1),
+    (1,2),
+    (2,5),
+    (2,3),
+    (1,4);
+
+-- question, user, questionfollow, reply, questionlike,  
+
+--     id INTEGER PRIMARY KEY,
+    -- question_id INTEGER NOT NULL,
+    -- reply_id INTEGER,
+    -- user_id INTEGER NOT NULL,
+    -- body TEXT NOT NULL,
--- questions.rb
+++ questions.rb
@@ -3,9 +3,9 @@ require 'singleton'
 
 
 class QuestionsDatabase < SQLite3::Database
-    include singleton
+    include Singleton
     def initialize
-        super(questions.db)
+        super('questions.db')
         self.type_translation = true
         self.results_as_hash = true
     end
@@ -25,11 +25,31 @@ class Question
         return Question.new(question[0])      
     end
 
+
+    def self.find_by_author_id(author_id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+            SELECT *
+            FROM questions
+            WHERE user_id = ?
+        SQL
+        question.map {|hash| Question.new(hash)}
+    end
+
+
     def initialize(attributes)
         @id = attributes['id']
         @title = attributes['title']
         @body = attributes['body']
         @user_id = attributes['user_id']
+    end
+
+
+    def author
+        User.find_by_id(user_id)
+    end
+
+    def replies
+        Reply.find_by_question_id(id)
     end
 end
 
@@ -62,6 +82,15 @@ class User
         @fname = attributes['fname']
         @lname = attributes['lname']
     end
+
+
+    def authored_questions
+        Question.find_by_author_id(id)
+    end
+
+    def authored_replies
+        Reply.find_by_user_id(id)
+    end
 end
 
 
@@ -89,6 +118,15 @@ class Reply
 
     attr_accessor :id, :question_id, :reply_id, :user_id, :body
 
+    def self.find_by_user_id(user_id)
+        replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT *
+            FROM replies
+            WHERE user_id = ?
+        SQL
+        replies.map {|hash| Reply.new(hash)}
+    end
+
     def self.find_by_id(id)
         reply = QuestionsDatabase.instance.execute(<<-SQL, id)
             SELECT *
@@ -98,12 +136,47 @@ class Reply
         return Reply.new(reply[0])      
     end
 
+    def self.find_by_question_id(question_id)
+        replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM replies
+            WHERE question_id = ?
+        SQL
+        replies.map {|hash| Reply.new(hash)}
+    end
+
     def initialize(attributes)
         @id = attributes['id']
         @question_id = attributes['question_id']
         @reply_id = attributes['reply_id']
         @user_id = attributes['user_id']
         @body = attributes['body']
+    end
+
+    def author
+        User.find_by_id(user_id)
+    end
+
+    def question
+        Question.find_by_id(question_id)
+    end
+
+    def parent_reply
+        if Reply.find_by_id(reply_id) == nil
+            raise "no parents"
+        else
+            return Reply.find_by_id(reply_id)
+        end
+    end
+
+    def child_replies
+        child_reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM replies
+            WHERE reply_id = ?
+        SQL
+
+        return Reply.new(child_reply.first)
     end
 end
 
